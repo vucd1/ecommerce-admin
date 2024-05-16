fdc1bab39dc0dacae66c2b9dc423e8cc68ce27da
vucd1 <64571700+vucd1@users.noreply.github.com>
2024-05-15 17:17:11 -0700

    Fixed store modal CommandItem bug 2

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,17 +1,5 @@
 import prismadb from "@/lib/prismadb";
 
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-  CommandShortcut,
-} from "@/components/ui/command";
-import { CalendarIcon } from "lucide-react";
-
 interface DashboardPageProps {
   params: { storeId: string}
 };
@@ -28,46 +16,6 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
   return (
     <div>
       Active Store: {store?. name}
-      <Command className="rounded-lg border shadow-md">
-      <CommandList>
-        <CommandInput placeholder="Type a command or search..." />
-      </CommandList>
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Suggestions">
-          <CommandItem>
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            <span>Calendar</span>
-          </CommandItem>
-          <CommandItem>
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            <span>Search Emoji</span>
-          </CommandItem>
-          <CommandItem>
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            <span>Launch</span>
-          </CommandItem>
-        </CommandGroup>
-        <CommandSeparator />
-        <CommandGroup heading="Settings">
-          <CommandItem>
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            <span>Profile</span>
-            <CommandShortcut>⌘P</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            <span>Mail</span>
-            <CommandShortcut>⌘B</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            <span>Settings</span>
-            <CommandShortcut>⌘S</CommandShortcut>
-          </CommandItem>
-        </CommandGroup>
-      </CommandList>
-    </Command>
     </div>
   )
 }
--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -110,7 +110,7 @@ export default function StoreSwitcher({
                     <CommandList>
                         <CommandGroup>
                             <CommandItem
-                                onClick={() => {
+                                onSelect={() => {
                                     setOpen(false);
                                     storeModal.onOpen();
                                 }}
